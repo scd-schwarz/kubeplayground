204d54b6cf7c313d361ebca0e83d59a618174a16
Daniel Schwarz <daniel-schwarz@posteo.net>
2023-08-20 13:52:53 +0200

    blub

--- terraform/example/main.tf
+++
@@ -1,9 +0,0 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/kube-master/.kube/config"
-}
-
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "terraform-example"
-  }
-}
